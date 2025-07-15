32ee29c49f5d3695a9c978a83a6543145aebb049
h076 <hazawood@me.com>
2025-07-15 13:52:43 +0100

    Initial structure.

---
+++ include/mongo/connect.hpp
@@ -0,0 +1,13 @@
+#ifndef CONNECT_H_
+#define CONNECT_H_
+
+namespace hjw {
+
+    namespace mongo {
+
+        void connect() {}
+
+    }
+}
+
+#endif // CONNECT_H_
---
+++ include/mongo/requests.hpp
@@ -0,0 +1,13 @@
+#ifndef REQUESTS_H_
+#define REQUESTS_H_
+
+namespace hjw {
+
+    namespace mongo {
+        void get() {};
+
+        void post() {};
+    }
+}
+
+#endif // REQUESTS_H_
