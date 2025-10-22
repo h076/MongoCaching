14ee141ecb9bdd2511510355d29969c63b12e954
h076 <hazawood@me.com>
2025-10-22 12:52:49 +0100

    New redis service for strategy logging.

---
+++ include/redis/StrategyService.hpp
@@ -0,0 +1,17 @@
+#ifndef STRATEGYSERVICE_H_
+#define STRATEGYSERVICE_H_
+
+namespace hjw {
+
+    namespace redis {
+
+        //namespace net = boost::asio;
+
+        class StrategyService {
+
+
+        };
+    }
+}
+
+#endif // STRATEGYSERVICE_H_
